396d0e4febec1f1311d35aad81331bd09218eca5
Nazariy Velychenko <n.velychenko@dev-branch.com>
2020-06-17 21:24:26 +0300

    OI-39: #comment Create IdeaInstateDeriver for Condition plugin for getting info about current state of idea. EntityIsModerated condition and two templates for transactions.

---
+++ modules/openideal_user/src/Plugin/RulesAction/UserTransactionBulkCreate.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Drupal\transaction\Plugin\RulesAction;
+
+use Drupal\rules\Core\RulesActionBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Provides the transaction create action.
+ *
+ * @RulesAction(
+ *   id = "openideal_user_transaction_bulk_create",
+ *   label = @Translation("Create a User points bulk transaction"),
+ *   category = @Translation("Transaction"),
+ *   context = {
+ *     @Todo: create logic.
+ *   }
+ * )
+ */
+class UserTransactionBulkCreate extends RulesActionBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * A logger instance.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected $logger;
+
+  /**
+   * Constructs an TransactionCreate object.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin ID for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Psr\Log\LoggerInterface $logger
+   *   A logger instance.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, LoggerInterface $logger) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->entityTypeManager = $entity_type_manager;
+    $this->logger = $logger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('entity_type.manager'),
+      $container->get('logger.factory')->get('rules')
+    );
+  }
+
+  /**
+   * Todo.
+   */
+  public function doExecute() {
+    // @Todo: create logic.
+  }
+
+}
