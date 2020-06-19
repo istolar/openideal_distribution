4d37642690d0ecad1eb1be3941d7e25600e6cd32
Nazariy Velychenko <n.velychenko@dev-branch.com>
2020-06-20 01:29:00 +0300

    OI-39: #comment Created RulesAction to give points to all user that are in an idea group.

--- modules/openideal_user/src/Plugin/RulesAction/TransactionBulkExecute.php
+++ modules/openideal_user/src/Plugin/RulesAction/TransactionBulkExecute.php
@@ -1,23 +1,22 @@
 <?php
 
-namespace Drupal\transaction\Plugin\RulesAction;
+namespace Drupal\openideal_user\Plugin\RulesAction;
 
+use Drupal\user\Entity\User;
+use Drupal\rules\Context\ContextConfig;
+use Drupal\group\GroupMembershipLoader;
 use Drupal\rules\Core\RulesActionBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\rules\Engine\ExpressionManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
-use Psr\Log\LoggerInterface;
 
 /**
  * Provides the transaction create action.
  *
  * @RulesAction(
- *   id = "openideal_user_transaction_bulk_execute",
- *   label = @Translation("Execute a User points bulk transaction"),
- *   category = @Translation("Transaction"),
- *   context = {
- *     @Todo: create logic.
- *   }
+ *   id = "openideal_user_potins_transaction_bulk_execute",
+ *   deriver = "Drupal\openideal_user\Plugin\RulesAction\TransactionBulkExecuteDeriver",
  * )
  */
 class TransactionBulkExecute extends RulesActionBase implements ContainerFactoryPluginInterface {
@@ -30,30 +29,34 @@ class TransactionBulkExecute extends RulesActionBase implements ContainerFactory
   protected $entityTypeManager;
 
   /**
-   * A logger instance.
+   * Expression manager.
    *
-   * @var \Psr\Log\LoggerInterface
+   * @var \Drupal\rules\Engine\ExpressionManager
    */
-  protected $logger;
+  protected $expressionManager;
 
   /**
-   * Constructs an TransactionCreate object.
+   * Group member ship loader.
    *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin ID for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Psr\Log\LoggerInterface $logger
-   *   A logger instance.
+   * @var \Drupal\group\GroupMembershipLoader
    */
-  public function __construct(array $configuration, $plugin_id, $plugin_definition, EntityTypeManagerInterface $entity_type_manager, LoggerInterface $logger) {
+  protected $groupMembershipLoader;
+
+  /**
+   * {@inheritDoc}
+   */
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    EntityTypeManagerInterface $entity_type_manager,
+    ExpressionManager $expressionManager,
+    GroupMembershipLoader $groupMembershipLoader
+  ) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->entityTypeManager = $entity_type_manager;
-    $this->logger = $logger;
+    $this->expressionManager = $expressionManager;
+    $this->groupMembershipLoader = $groupMembershipLoader;
   }
 
   /**
@@ -65,15 +68,69 @@ class TransactionBulkExecute extends RulesActionBase implements ContainerFactory
       $plugin_id,
       $plugin_definition,
       $container->get('entity_type.manager'),
-      $container->get('logger.factory')->get('rules')
+      $container->get('plugin.manager.rules_expression'),
+      $container->get('group.membership_loader'),
     );
   }
 
   /**
-   * Todo.
+   * {@inheritDoc}
    */
-  public function doExecute() {
-    // @Todo: create logic.
+  public function execute() {
+    $users = $this->getUsers();
+    // Create a actions for every user and grant them points.
+    foreach ($users as $group_member) {
+      // Loop the transaction create and execute actions per user in group.
+      $rules_rule = $this->expressionManager->createRule();
+      $rules_rule->addAction('transaction_create:userpoints', $this->prepareContextForTransactionCreate($group_member));
+      $context_config = ContextConfig::create()
+        ->setValue('immediate', TRUE)
+        ->map('transaction', 'transaction');
+      $rules_rule->addAction('transaction_execute', $context_config);
+      $rules_rule->execute();
+    }
+  }
+
+  /**
+   * Get users in group from node.
+   */
+  private function getUsers() {
+    $users = [];
+    $storage = $this->entityTypeManager->getStorage('group_content');
+    /** @var \Drupal\group\Entity\GroupContent $group_content */
+    $group_contents = $storage->loadByEntity($this->getContextValue('idea'));
+    $group_content = reset($group_contents);
+    $group_members_ships = $this->groupMembershipLoader->loadByGroup($group_content->getGroup());
+    foreach ($group_members_ships as $content_group) {
+      $member = $content_group->getUser();
+      $users += [$member->id() => $member];
+    }
+    return $users;
+  }
+
+  /**
+   * Prepare context for transaction create.
+   *
+   * @param \Drupal\user\Entity\User $user
+   *   User.
+   *
+   * @return \Drupal\rules\Context\ContextConfig
+   *   Config.
+   */
+  private function prepareContextForTransactionCreate(User $user) {
+    $context_config = ContextConfig::create();
+    foreach ($this->getContexts() as $context_name => $context) {
+      $value = $this->getContextValue($context_name);
+      if ($context_name == 'idea') {
+        continue;
+      }
+      if ($context_name == 'target_entity') {
+        $context_config->setValue($context_name, $user);
+        continue;
+      }
+      $context_config->setValue($context_name, $value);
+    }
+    return $context_config;
   }
 
 }
