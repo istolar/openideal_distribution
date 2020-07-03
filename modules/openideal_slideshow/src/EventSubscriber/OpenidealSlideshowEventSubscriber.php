cf3e9ce1fe63d6b4b39d26b3edd4271a0d44fc32
Nazariy Velychenko <n.velychenko@dev-branch.com>
2020-07-03 06:33:22 +0300

    OI-71: Created the openideal_slideshow module, block slideshow, and event subscriber.

---
+++ modules/openideal_slideshow/src/EventSubscriber/OpenidealSlideshowEventSubscriber.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Drupal\openideal_slideshow\EventSubscriber;
+
+use Drupal\Core\Messenger\MessengerTrait;
+use Drupal\layout_builder\Event\SectionComponentBuildRenderArrayEvent;
+use Drupal\layout_builder\LayoutBuilderEvents;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+/**
+ * Class OpenidealIdeaEventSubscriber.
+ */
+class OpenidealSlideshowEventSubscriber implements EventSubscriberInterface {
+
+  use MessengerTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    // Make sure to react on event before layout_builder.
+    // @see BlockComponentRenderArray
+    $events[LayoutBuilderEvents::SECTION_COMPONENT_BUILD_RENDER_ARRAY] = ['onBuildRender', 101];
+    return $events;
+  }
+
+  /**
+   * Add max-age to overall_score field.
+   *
+   * @param \Drupal\layout_builder\Event\SectionComponentBuildRenderArrayEvent $event
+   *   Event.
+   */
+  public function onBuildRender(SectionComponentBuildRenderArrayEvent $event) {
+    $context = $event->getContexts();
+    if ($event->getPlugin()->getPLuginId() == 'openidel_slideshow_block' && isset($context['entity'])) {
+      /** @var \Drupal\openideal_slideshow\Plugin\Block\Slideshow $plugin */
+      $plugin = $event->getPlugin();
+
+      // Get the node from context, and prepare field for the slideshow plugin.
+      $node = $context['entity']->getContextValue();
+      /** @var \Drupal\file\Plugin\Field\FieldType\FileFieldItemList  $images */
+      $images = $node->field_images;
+
+      if ($node->bundle() == 'challenge') {
+        $images->appendItem($node->field_main_image->first()->entity);
+      }
+      $plugin->setConfigurationValue('images', $images);
+    }
+  }
+
+}
