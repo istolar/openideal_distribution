0999be109c1760250538d11487e9f1d87947e818
Nazariy Velychenko <n.velychenko@dev-branch.com>
2020-07-15 22:59:32 +0300

    OI-83: Removed useless code.

--- modules/openideal_slideshow/src/Plugin/Block/Slideshow.php
+++ modules/openideal_slideshow/src/Plugin/Block/Slideshow.php
@@ -3,10 +3,6 @@
 namespace Drupal\openideal_slideshow\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
-use Drupal\slick\SlickFormatter;
-use Drupal\slick\SlickManager;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'Slideshow' block.
@@ -17,56 +13,7 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *   category = @Translation("Openideal")
  * )
  */
-class Slideshow extends BlockBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * Slick manager service.
-   *
-   * @var \Drupal\slick\SlickManager
-   */
-  protected $slickManager;
-
-  /**
-   * Slick formatter service.
-   *
-   * @var \Drupal\slick\SlickFormatter
-   */
-  protected $slickFormatter;
-
-  /**
-   * Constructs a new Slideshow.
-   *
-   * @param array $configuration
-   *   A configuration array containing information about the plugin instance.
-   * @param string $plugin_id
-   *   The plugin ID for the plugin instance.
-   * @param mixed $plugin_definition
-   *   The plugin implementation definition.
-   * @param \Drupal\slick\SlickManager $slick_manager
-   *   Slick manager.
-   * @param \Drupal\slick\SlickFormatter $slick_formatter
-   *   Slick formatter manager.
-   */
-  public function __construct(array $configuration,
-    $plugin_id,
-    $plugin_definition,
-    SlickManager $slick_manager,
-    SlickFormatter $slick_formatter
-  ) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-    $this->slickManager = $slick_manager;
-    $this->slickFormatter = $slick_formatter;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static($configuration, $plugin_id, $plugin_definition,
-      $container->get('slick.manager'),
-      $container->get('slick.formatter')
-    );
-  }
+class Slideshow extends BlockBase {
 
   /**
    * {@inheritDoc}
