c6b10126af637e36eca1af4aca1d7468938aa84f
Nazariy Velychenko <n.velychenko@dev-branch.com>
2020-08-10 10:53:27 +0300

    OI-111: Replaced configuration from block to the global module's configuration form.

--- modules/openideal_footer/src/Plugin/Block/OpenidealFooterPoweredByBlock.php
+++ modules/openideal_footer/src/Plugin/Block/OpenidealFooterPoweredByBlock.php
@@ -4,7 +4,6 @@ namespace Drupal\openideal_footer\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Config\ConfigFactory;
-use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Theme\ThemeManager;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -19,11 +18,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * )
  */
 class OpenidealFooterPoweredByBlock extends BlockBase implements ContainerFactoryPluginInterface {
-
-  /**
-   * The openideal official site.
-   */
-  const OPENIDEAL_OFFICIAL_SITE = 'https://www.openidealapp.com/';
 
   /**
    * Theme manger.
@@ -59,48 +53,20 @@ class OpenidealFooterPoweredByBlock extends BlockBase implements ContainerFactor
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static($configuration, $plugin_id, $plugin_definition,
       $container->get('theme.manager'),
-      $container->get('config.factory'),
+      $container->get('config.factory')
     );
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function blockForm($form, FormStateInterface $form_state) {
-    $form = parent::blockForm($form, $form_state);
-    $config = $this->getConfiguration();
-
-    $form['openideal_official_site'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Official site'),
-      '#description' => $this->t('The OpenideaL official site.'),
-      '#default_value' => $config['openideal_official_site'] ?? self::OPENIDEAL_OFFICIAL_SITE,
-      '#required' => TRUE,
-    ];
-
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public function blockSubmit($form, FormStateInterface $form_state) {
-    parent::blockSubmit($form, $form_state);
-    $values = $form_state->getValues();
-    $this->configuration['openideal_official_site'] = $values['openideal_official_site'];
   }
 
   /**
    * {@inheritDoc}
    */
   public function build() {
-    $block_config = $this->getConfiguration();
     $config = $this->configFactory->get('openideal_footer.openideal_footer_links_config');
     $path = $this->themeManager->getActiveTheme()->getPath();
     $base_theme_path = base_path() . $path;
     return [
       '#theme' => 'openideal_powered_by',
-      '#site_url' => $block_config['openideal_official_site'],
+      '#site_url' => $config->get('openideal_official_site'),
       '#logo' => $base_theme_path . '/misc/icons/logo_openideal.png',
       '#links' => [
         'github' => [
