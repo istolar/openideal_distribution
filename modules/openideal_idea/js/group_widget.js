f6b552ed9248ea0373aed8478c83735e07b5127d
Nazariy Velychenko <n.velychenko@dev-branch.com>
2020-08-17 11:44:02 +0300

    OI-95 Changed logic to not unset but simply hide group roles options.

--- modules/openideal_idea/js/group_widget.js
+++ modules/openideal_idea/js/group_widget.js
@@ -7,6 +7,9 @@
   Drupal.behaviors.openidealUserGroupWidget = {
     attach: function (context, settings) {
       $('#edit-group-roles .form-item-group-roles-idea-author').addClass('visually-hidden')
+      if (settings.openidealIdea.hideExperts) {
+        $('#edit-group-roles .form-item-group-roles-idea-expert').addClass('visually-hidden')
+      }
     }
   };
 })(jQuery, Drupal);
