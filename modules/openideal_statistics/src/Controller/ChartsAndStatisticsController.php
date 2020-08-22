e16278b317531ecfee7619c63686a34e1c72f23b
Nazariy Velychenko <n.velychenko@dev-branch.com>
2020-08-22 20:48:08 +0300

    OI-124: Created the user account block, and added the logic to show login link if user is not authenticated.

--- modules/openideal_statistics/src/Controller/ChartsAndStatisticsController.php
+++ modules/openideal_statistics/src/Controller/ChartsAndStatisticsController.php
@@ -3,8 +3,6 @@
 namespace Drupal\openideal_statistics\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\Core\Block\BlockManagerInterface;
 
 /**
  * Class ChartsAndStatisticsController.
@@ -12,40 +10,10 @@ use Drupal\Core\Block\BlockManagerInterface;
 class ChartsAndStatisticsController extends ControllerBase {
 
   /**
-   * Drupal\Core\Block\BlockManagerInterface definition.
-   *
-   * @var \Drupal\Core\Block\BlockManagerInterface
-   */
-  protected $pluginManagerBlock;
-
-  /**
-   * Constructs a new ChartsAndStatisticsController object.
-   *
-   * @param \Drupal\Core\Block\BlockManagerInterface $plugin_manager_block
-   *   Plugin manager block.
-   */
-  public function __construct(BlockManagerInterface $plugin_manager_block) {
-    $this->pluginManagerBlock = $plugin_manager_block;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('plugin.manager.block')
-    );
-  }
-
-  /**
    * Charts.
-   *
-   * @return string
-   *   Return Hello string.
    */
   public function charts() {
-    $charts = $this->pluginManagerBlock->createInstance('openideal_statistics_charts_block');
-    return $charts->build();
+    return [];
   }
 
 }
