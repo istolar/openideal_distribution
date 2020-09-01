fe0be91a5c5a9916c5dad2a751e8851a1b8bd2a2
Nazariy Velychenko <n.velychenko@dev-branch.com>
2020-09-01 19:23:40 +0300

    OI-174,151,176: Reworked the OpenidealIdeaUpdateInfo block, implemented ability to hide any of blocks elements, and inserted block into the teaser mods of the Challenge, re-themed teaser mods for with that block. Fixed the messages Partial positions in the view modes. Created a trait to get that provide ability to get contextual entity, and applied to all blocks. Fixed bug on frontpage when images were load after masonry started to work. Added title attribute to the icons. Reworked the site-branding template. And placed that block instead of custom one to the front page.

---
+++ modules/openideal_challenge/src/OpenidealContextEntityTrait.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\openideal_challenge;
+
+/**
+ * Provide trait to get contextual entity and verify it for layout_builder view.
+ *
+ * @package Drupal\openideal_challenge
+ */
+trait OpenidealContextEntityTrait {
+
+  /**
+   * Get the entity from contexts.
+   *
+   * @param array $contexts
+   *   Contexts.
+   * @param string $entity_type
+   *   The entity type.
+   *
+   * @Todo: create an interface with constants those describe $entity_type's?
+   *
+   * @return \Drupal\Core\Entity\EntityInterface|false
+   *   Entity.
+   */
+  protected function getEntity(array $contexts, string $entity_type = 'node') {
+    if (isset($contexts[$entity_type])
+      && ($entity = $contexts[$entity_type]->getContextValue())
+      && !$entity->isNew()) {
+      return $entity;
+    }
+    return FALSE;
+  }
+
+}
