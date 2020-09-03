14197496885ea846872e2abc449239c959678456
Nazariy Velychenko <n.velychenko@dev-branch.com>
2020-09-03 15:47:09 +0300

    OI-165: Implemented the filters form validation and submition. Created OpenidealStatisticsFilterTrait trait that provide method for all charts block ecosystem, to get filters from GET params. Implemented standardized getData method for all charts blocks, and reworked block to react on filters. Themed filters.

--- modules/openideal_statistics/src/Form/OpenidealStatisticsDateSelectForm.php
+++ modules/openideal_statistics/src/Form/OpenidealStatisticsDateSelectForm.php
@@ -16,6 +16,8 @@ class OpenidealStatisticsDateSelectForm extends FormBase {
    */
   const FIXED_RANGE = 'range';
   const DATE_TYPE = 'date_type';
+  const DATE_TYPE_CUSTOM = 'custom';
+  const DATE_TYPE_FIXED = 'fixed';
   const FROM = 'from';
   const TO = 'to';
 
@@ -45,34 +47,50 @@ class OpenidealStatisticsDateSelectForm extends FormBase {
       $options[$month] = $this->formatPlural($month, '1 month', '@count months');
     }
 
-    $form['date_configuration'][self::DATE_TYPE] = [
+    $form['filters_wrapper'] = [
+      '#type' => 'container',
+      '#attributes' => ['class' => ['reports-filters']],
+    ];
+    $form['filters_wrapper'][self::DATE_TYPE] = [
+      '#title' => $this->t('Date type'),
       '#type' => 'radios',
+      '#attributes' => ['class' => ['reports-filters--date-type']],
       '#options' => [
-        'fixed' => $this->t('From'),
-        'custom' => $this->t('From'),
+        'fixed' => $this->t('Fixed filter'),
+        'custom' => $this->t('Range filter'),
       ],
-      '#default_value' => $query->get(self::DATE_TYPE) ?? '',
+      '#default_value' => $query->get(self::DATE_TYPE) ?? NULL,
       '#required' => TRUE,
     ];
-    $form['date_configuration'][self::FIXED_RANGE] = [
+    $form['filters_wrapper']['filter_container'] = [
+      '#type' => 'container',
+      '#attributes' => ['class' => ['reports-filters--filters']],
+      'fixed_dates' => [
+        '#type' => 'container',
+        '#attributes' => ['class' => ['reports-filters--filters__custom_dates']],
+      ],
+    ];
+
+    $form['filters_wrapper']['filter_container']['fixed_dates'][self::FIXED_RANGE] = [
       '#type' => 'select',
-      '#title' => $this->t('From'),
+      '#prefix' => '<div class="reports-filters--filters__fixed_dates--label">' . $this->t('From') . '</div>',
       '#options' => $options,
-      '#default_value' => $query->get(self::FIXED_RANGE) ?? '',
+      '#default_value' => $query->get(self::FIXED_RANGE) ?? NULL,
     ];
-    $form['date_configuration']['custom_dates'] = [
+
+    $form['filters_wrapper']['filter_container']['custom_dates'] = [
       '#type' => 'fieldset',
-      '#attributes' => ['class' => ['custom_dates_w']],
+      '#attributes' => ['class' => ['reports-filters--filters__custom_dates']],
     ];
-    $form['date_configuration']['custom_dates'][self::FROM] = [
+    $form['filters_wrapper']['filter_container']['custom_dates'][self::FROM] = [
       '#type' => 'date',
-      '#title' => $this->t('From'),
-      '#default_value' => $query->get(self::FROM) ?? '',
+      '#prefix' => '<div class="reports-filters--filters__custom_dates--label">' . $this->t('From') . '</div>',
+      '#default_value' => $query->get(self::FROM) ?? NULL,
     ];
-    $form['date_configuration']['custom_dates'][self::TO] = [
+    $form['filters_wrapper']['filter_container']['custom_dates'][self::TO] = [
       '#type' => 'date',
-      '#title' => $this->t('To'),
-      '#default_value' => $query->get(self::TO) ?? '',
+      '#prefix' => '<div class="reports-filters--filters__custom_dates--label to">' . $this->t('To') . '</div>',
+      '#default_value' => $query->get(self::TO) ?? NULL,
     ];
 
     $form['submit'] = [
@@ -87,12 +105,18 @@ class OpenidealStatisticsDateSelectForm extends FormBase {
    * {@inheritdoc}
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
-    $values = $form_state->getValue('date_configuration');
-    if ($values[self::DATE_TYPE] == 'fixed' && !$values[self::FIXED_RANGE]) {
+    $values = $form_state->getValues();
+    if ($values[self::DATE_TYPE] == self::DATE_TYPE_FIXED && !$values[self::FIXED_RANGE]) {
       $form_state->setErrorByName(self::FIXED_RANGE, $this->t('An option should be selected'));
     }
-    if ($values[self::DATE_TYPE] == 'custom' && (!$values['custom_dates'][self::FROM] ||!$values['custom_dates'][self::TO])) {
-      $form_state->setErrorByName(self::FIXED_RANGE, $this->t('You must specify both dates'));
+    elseif ($values[self::DATE_TYPE] == self::DATE_TYPE_CUSTOM && (!$values[self::FROM] && !$values[self::TO])) {
+      $form_state->setErrorByName('', $this->t('You must specify both dates'));
+    }
+    elseif ($values[self::DATE_TYPE] == self::DATE_TYPE_CUSTOM && !$values[self::FROM]) {
+      $form_state->setErrorByName(self::FROM, $this->t('Field cannot be empty'));
+    }
+    elseif ($values[self::DATE_TYPE] == self::DATE_TYPE_CUSTOM && !$values[self::TO]) {
+      $form_state->setErrorByName(self::TO, $this->t('Field cannot be empty'));
     }
   }
 
@@ -100,7 +124,23 @@ class OpenidealStatisticsDateSelectForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
+    $values = $form_state->getValues();
+    $query = $this->requestStack->getCurrentRequest()->query;
+    // Clear query.
+    $query->replace([]);
+    // Set query params depending on filters.
+    switch ($values[self::DATE_TYPE]) {
+      case self::DATE_TYPE_FIXED:
+        $query->set(self::FIXED_RANGE, $values[self::FIXED_RANGE]);
+        $query->set(self::DATE_TYPE, $values[self::DATE_TYPE]);
+        break;
 
+      case self::DATE_TYPE_CUSTOM:
+        $query->set(self::DATE_TYPE, $values[self::DATE_TYPE]);
+        $query->set(self::FROM, $values[self::FROM]);
+        $query->set(self::TO, $values[self::TO]);
+        break;
+    }
   }
 
 }
